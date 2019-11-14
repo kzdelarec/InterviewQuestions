d439beae4d3f8b4e684e4ee82a79a736dc09239b
Kristijan Zdelarec <kristijan.zdelarec@gmail.com>
2019-11-14 22:03:23 +0100

    Fibonacci O(2^n)

---
+++ src/interviewQuestions/Fibonacci/Main.java
@@ -0,0 +1,21 @@
+package interviewQuestions.Fibonacci;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(fibonacci(10)); //55
+	}
+	
+	public static int fibonacci(int n) {
+		
+		if(n <= 0) {
+			return 0;
+		} else if( n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+		
+	}
+
+}
